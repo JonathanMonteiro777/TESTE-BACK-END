77b9285de89ed86d89568b0a39239de784dcc04a
JonathanMonteiro777 <167100917+JonathanMonteiro777@users.noreply.github.com>
2025-06-17 23:23:38 -0300

    Primeiro commit das atividades de testes de back-end

---
+++ TESTE_EXEMPLO/multi_teste_xunit/Operacoes.cs
@@ -0,0 +1,18 @@
+namespace multi_teste_xunit
+{
+    public class Operacoes
+    {
+        public double Somar(double primeiroNumero, double
+        segundoNumero)
+        {
+        return (primeiroNumero + segundoNumero);
+        }
+        
+        public double Multiplicar(double primeiroNumero,
+        double segundoNumero)
+        {
+        return (primeiroNumero * segundoNumero);
+        }
+    }
+}
+
